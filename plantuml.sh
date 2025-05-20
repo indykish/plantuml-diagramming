6ce5ba1314f97dc8b8f81641df933129cbb82402
Kishore Kumar <kishore.kumar@e2enetworks.com>
2025-05-20 17:02:49 +0530

    fix to load libraries from local

--- plantuml.sh
+++ plantuml.sh
@@ -7,4 +7,4 @@ echo $$
 podman rm -f plantuml 2>/dev/null
 
 # Start the PlantUML server using Podman
-podman run --env PLANTUML_SECURITY_PROFILE=LEGACY --name plantuml -p 6200:8080 plantuml/plantuml-server:tomcat-v1.2024.4
+podman run -v $(pwd)/libraries:/libraries --env PLANTUML_SECURITY_PROFILE=LEGACY --name plantuml -p 6200:8080  plantuml/plantuml-server:tomcat-v1.2024.4
