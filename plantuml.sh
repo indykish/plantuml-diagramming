cba46bab127d7cbf35c682628c67cb8fd30f05c8
Kishore Kumar <kishore.kumar@e2enetworks.com>
2025-07-29 16:43:49 +0530

    moved back to rancher desktop (docker)

--- plantuml.sh
+++ plantuml.sh
@@ -4,7 +4,7 @@
 echo $$
 
 # Remove existing container if it exists
-podman rm -f plantuml 2>/dev/null
+docker rm -f plantuml 2>/dev/null
 
-# Start the PlantUML server using Podman
-podman run -v $(pwd)/libraries:/libraries --env PLANTUML_SECURITY_PROFILE=LEGACY --name plantuml -p 6200:8080  plantuml/plantuml-server:tomcat-v1.2024.4
+# Start the PlantUML server using Docker
+docker run -v $(pwd)/libraries:/libraries --env PLANTUML_SECURITY_PROFILE=LEGACY --name plantuml -p 6200:8080  plantuml/plantuml-server:tomcat
